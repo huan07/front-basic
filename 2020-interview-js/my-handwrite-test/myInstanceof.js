8a3d12821c91b9c1e2255dfcc30077c08675e526
huan07 <845265003@qq.com>
2022-02-06 22:26:10 +0800

    fix

--- 2020-interview-js/my-handwrite-test/myInstanceof.js
+++ 2020-interview-js/my-handwrite-test/myInstanceof.js
@@ -1,32 +1,29 @@
-(function() {
-    var myInstanceof = function(obj, constructor) {
-        var _prototype = constructor.prototype;
+(function () {
+  var myInstanceof = function (obj, constructor) {
+    var _prototype = constructor.prototype;
 
-        while (obj) {
-            if (obj === _prototype) {
-                return true;
-            }
-            obj = Object.getPrototypeOf(obj);
-        }
-        return false;
-    };
+    while (obj) {
+      // ? 是否需要添加条件 typeof obj === 'object'
+      if (obj === _prototype) {
+        return true;
+      }
+      obj = Object.getPrototypeOf(obj);
+    }
+    return false;
+  };
 
-    // test x3
-    console.warn('test myInstanceof = = = = = = = = = = = = = = = = = = =');
+  var arr = [];
+  var obj = {};
 
-    var arr = [];
-    var obj = {};
+  var result = myInstanceof(arr, Array);
+  var result_2 = myInstanceof(arr, Object);
 
-    var result = myInstanceof(arr, Array);
-    var result_2 = myInstanceof(arr, Object);
+  var result2 = myInstanceof(obj, Object);
 
-    var result2 = myInstanceof(obj, Object);
+  var result3 = myInstanceof(null, Object);
 
-    var result3 = myInstanceof(null, Object);
+  var result4 = myInstanceof('', Object);
 
-    var result4 = myInstanceof('', Object);
-
-    console.log('myInstanceof result =>', result, result_2, result2, result3);
-    console.log(result4);
-
+  console.log('myInstanceof result =>', result, result_2, result2);
+  console.log(result3, result4);
 })();
--- 2020-interview-js/my-handwrite-test/myNew.js
+++ 2020-interview-js/my-handwrite-test/myNew.js
@@ -1,32 +1,22 @@
-(function() {
-    function myNew(constructor, ...args) {
+(function () {
+  function myNew(constructor, ...args) {
+    const context = Object.create(constructor.prototype);
 
-        const context = Object.create(constructor.prototype);
+    const result = constructor.apply(context, args);
 
-        const result = constructor.apply(context, args);
+    return typeof result === 'object' && result !== null ? result : context;
+  }
 
-        return typeof result === 'object' && result !== null ? result : context;
-    }
+  function Person(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-    // test x3
-    console.warn('test myNew = = = = = = = = = = = = = = = = = = =');
+  function getMessage() {
+    return 'this is a message';
+  }
 
-    function Person(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    var test = myNew(Person, 'yh', 32);
-    var person = new Person('yh', 32);
-
-    function getMessage() {
-        return 'this is a message';
-    }
-
-    console.log('myNew =>', test);
-    console.log('new Person() =>', person);
-    console.log('myNew getMessage =>', myNew(getMessage));
-
+  console.log('myNew =>', myNew(Person, 'yh', 32));
+  console.log('new Person() =>', new Person('yh', 32));
+  console.log('myNew getMessage =>', myNew(getMessage));
 })();
-
-
